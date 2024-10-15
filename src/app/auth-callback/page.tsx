3ce4790e37614e63b2569621725db52185d0ac96
Aritra <aritrasarkar00007@gmail.com>
2024-10-15 17:33:40 +0530

    trpc tweak

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,16 +1,36 @@
-import { useRouter, useSearchParams } from 'next/navigation'
-import React from 'react'
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect } from 'react';
+import { trpc } from '../_trpc/client';
+import { Loader2 } from 'lucide-react';
 
-const page = () => {
-    const router = useRouter()
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get('origin');
 
-    const searchParams = useSearchParams()
-    const origin = searchParams.get('origin')
+  const { data, error, isSuccess } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
 
-    
+  useEffect(() => {
+    if (isSuccess && data?.success) {
+      // User is synced to the database
+      router.push(origin ? `/${origin}` : '/dashboard');
+    } else if (error?.data?.code === 'UNAUTHORIZED') {
+      router.push('/sign-in');
+    }
+  }, [isSuccess, data, error, origin, router]);
+
   return (
-    <div>page</div>
-  )
-}
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Page;
