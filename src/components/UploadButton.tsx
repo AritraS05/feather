298fba8ee3aee5adc85c2a31e12d959d901b9fe9
Aritra <aritrasarkar00007@gmail.com>
2024-10-17 11:38:56 +0530

    upload functionality

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,7 +1,32 @@
 "use client"
 import React, { useState } from 'react'
-import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from './ui/dialog'
 import { Button } from './ui/button'
+
+import Dropzone from "react-dropzone"
+
+const UploadDropzone = () =>{
+    return (
+    <Dropzone multiple={false} onDrop={(acceptedFile) =>{
+        console.log(acceptedFile);
+    }}>
+        {({getRootProps, getInputProps,acceptedFiles}) =>(
+            <div {...getRootProps()} className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
+                <div className="flex items-center justify-center h-full w-full">
+                    <label 
+                        htmlFor="dropzone-file"
+                        className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'
+                    >
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            example.pdf
+                        </div>
+                    </label>
+                </div>
+            </div>
+        )}
+    </Dropzone>
+    )
+}
 
 const UploadButton = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -17,7 +42,8 @@ const UploadButton = () => {
                 </Button>
         </DialogTrigger>
         <DialogContent>
-            test example
+            <DialogTitle>Upload Your File</DialogTitle>
+            <UploadDropzone />
         </DialogContent>
     </Dialog>
   )
